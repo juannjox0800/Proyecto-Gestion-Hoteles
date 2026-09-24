0059ddc2b284092d87ffa9c537b0d05022e34459
TahielRupayan <tahielrupayan@gmail.com>
2026-09-23 22:58:11 -0300

    Update RegistrarHabitacion.java

--- FRONTEND/src/ar/edu/unrn/seminario/gui/RegistrarHabitacion.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/RegistrarHabitacion.java
@@ -13,6 +13,7 @@ import javax.swing.JList;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import java.awt.Font;
 
 public class RegistrarHabitacion extends JFrame {
 
@@ -43,6 +44,7 @@ public class RegistrarHabitacion extends JFrame {
 	 * Create the frame.
 	 */
 	public RegistrarHabitacion() {
+		setTitle("Registrar Habitacion");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -50,57 +52,59 @@ public class RegistrarHabitacion extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("N° Habitación");
-		lblNewLabel.setBounds(10, 23, 70, 25);
+		JLabel lblNewLabel = new JLabel("N° Habitación:");
+		lblNewLabel.setBounds(24, 23, 90, 25);
 		contentPane.add(lblNewLabel);
 		
-		JLabel numeroCamas = new JLabel("Número de camas");
-		numeroCamas.setBounds(10, 59, 90, 25);
+		JLabel numeroCamas = new JLabel("Número de camas:");
+		numeroCamas.setBounds(24, 59, 104, 25);
 		contentPane.add(numeroCamas);
 		
-		JLabel tipoDeCamas = new JLabel("Tipo de camas");
-		tipoDeCamas.setBounds(10, 93, 90, 25);
+		JLabel tipoDeCamas = new JLabel("Tipo de camas:");
+		tipoDeCamas.setBounds(24, 93, 90, 25);
 		contentPane.add(tipoDeCamas);
 		
-		JLabel tipoHabitacion = new JLabel("Tipo Habitacion");
-		tipoHabitacion.setBounds(10, 129, 78, 25);
+		JLabel tipoHabitacion = new JLabel("Tipo Habitacion:");
+		tipoHabitacion.setBounds(24, 129, 90, 25);
 		contentPane.add(tipoHabitacion);
 		
 		textField = new JTextField();
-		textField.setBounds(124, 25, 86, 20);
+		textField.setBounds(145, 25, 183, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(124, 61, 86, 20);
+		textField_1.setBounds(145, 61, 183, 20);
 		contentPane.add(textField_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(124, 95, 86, 20);
+		textField_2.setBounds(145, 95, 183, 20);
 		contentPane.add(textField_2);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Estandar", "Superior", "Deluxe", "Suite"}));
-		comboBox.setBounds(124, 130, 86, 22);
+		comboBox.setBounds(145, 130, 183, 22);
 		contentPane.add(comboBox);
 		
-		JLabel costoHabitacion = new JLabel("Costo Habitacion");
-		costoHabitacion.setBounds(10, 165, 78, 25);
+		JLabel costoHabitacion = new JLabel("Costo Habitacion:");
+		costoHabitacion.setBounds(24, 165, 104, 25);
 		contentPane.add(costoHabitacion);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(124, 167, 86, 20);
+		textField_3.setBounds(145, 167, 183, 20);
 		contentPane.add(textField_3);
 		
 		JButton btnNewButton = new JButton("Cancelar");
-		btnNewButton.setBounds(345, 227, 89, 23);
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton.setBounds(338, 227, 89, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Aceptar");
-		btnNewButton_1.setBounds(246, 227, 89, 23);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnNewButton_1.setBounds(239, 227, 89, 23);
 		contentPane.add(btnNewButton_1);
 
 	}
